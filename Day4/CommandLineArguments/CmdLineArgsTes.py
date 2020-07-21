71fc0ceae0412b652fcc0b07e0f77e25d2ffdc72
viswanathrba <35405583+viswanathrba@users.noreply.github.com>
2020-07-21 08:58:53 +0530

    Add files via upload

---
+++ Day4/CommandLineArguments/CmdLineArgsTes.py
@@ -0,0 +1,9 @@
+import sys
+
+print(sys.argv)
+
+FirstCmd = sys.argv[1]
+SecondCmd = sys.argv[2]
+
+print(FirstCmd)
+print(SecondCmd)
---
+++ Day4/CommandLineArguments/PassWordCommandLine.py
@@ -0,0 +1,8 @@
+import getpass
+
+Username = input("Please enter your username : ")
+Password = getpass.getpass()
+
+
+print(Username)
+print(Password)
