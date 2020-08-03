baa5c3d3dd9c5da00aba8f433a57e3affb052410
anjan4273 <68360180+anjan4273@users.noreply.github.com>
2020-08-03 07:34:47 +0530

    Update OOPsTypes.py

--- Day 8/14OOPS/OOPsTypes.py
+++ Day 8/14OOPS/OOPsTypes.py
@@ -2,9 +2,8 @@ OOPs conceps:
 1) Normal Class
 2) Inheritance
 3) Multiple Inheritance
-4) Super class method
-5) Polymorphism
-6) Encapsulation
-7) Class Method
-8) Statis Method
-9) Abstract Method or ABC method
+4) Polymorphism Using Super class method
+5) Encapsulation
+6) Class Method
+7) Statis Method
+8) Abstract Method or ABC method
